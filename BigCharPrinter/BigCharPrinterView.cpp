ae4ec3c6c378b4a9cd87e7b271492271124e33da
dxphmj <huangmingji@ustb.edu.cn>
2019-11-26 01:46:38 +0800

    初步实现了打印编辑的功能1

--- BigCharPrinter/BigCharPrinterView.cpp
+++ BigCharPrinter/BigCharPrinterView.cpp
@@ -51,15 +51,6 @@ CBigCharPrinterView::~CBigCharPrinterView()
 void CBigCharPrinterView::DoDataExchange(CDataExchange* pDX)
 {
 	CFormView::DoDataExchange(pDX);
-	DDX_Control(pDX, IDC_BUT_PRINT_EDIT, m_ButPrintEdit);
-	DDX_Control(pDX, IDC_BUT_PRINT_PARAM, m_ButPrintParam);
-	DDX_Control(pDX, IDC_BUT_CLEAN, m_ButPrintClean);
-	DDX_Control(pDX, IDC_BUT_PRINT_CONTROL, m_ButPrintControl);
-	DDX_Control(pDX, IDC_BUT_COUNTER, m_ButPrintCounter);
-	DDX_Control(pDX, IDC_BUT_ICON, m_ButPrintIcon);
-	DDX_Control(pDX, IDC_BUT_CLASS_SETTING, m_ButPrintClass);
-	DDX_Control(pDX, IDC_BUT_SYSTEM, m_ButPrintSystem);
-	DDX_Control(pDX, IDC_BUT_PRINTER_MANAGE, m_ButPrinterManage);
 }
 
 BOOL CBigCharPrinterView::PreCreateWindow(CREATESTRUCT& cs)
@@ -96,7 +87,7 @@ void CBigCharPrinterView::OnInitialUpdate()
 	
 	int nX = 50;
 	int nY = 140;
-	int nWidth = 800;
+	int nWidth = 1000;
 	int nHeight = 450;
 
 	m_dlgPrintEdit->Create(IDD_DIA_PRINT_EDIT,this);
@@ -135,54 +126,80 @@ void CBigCharPrinterView::OnInitialUpdate()
 	m_dlgPrinterManage->SetFont(&afxGlobalData.fontDefaultGUIBold);
 	m_dlgPrinterManage->MoveWindow(nX,nY,nWidth,nHeight);
 
-	//m_tab.AddTab(m_dlgPrintEdit,L"打印编辑");
-	//m_tab.AddTab(m_dlgPrintControl,L"打印设置");
-	//m_tab.AddTab(m_dlgMouthClean,L"喷头清洗");
-	//m_tab.AddTab(m_dlgPrintParam,L"打印变量");
-	//m_tab.AddTab(m_dlgCustomIcon,L"图案编辑");
-	//m_tab.AddTab(m_dlgSystemSetting,L"系统设置");
-
-
-	//m_tab.EnableActiveTabCloseButton();//是否添加关闭选项卡按钮
-//	m_tab.SetActiveTab(0); //激活选项卡,以当前选项卡为第一页。
-
-	m_ButPrintEdit.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	//彩色图标设置
+	m_ButPrintEdit.SubclassDlgItem(IDC_BUT_PRINT_EDIT,this);
+ 	m_ButPrintEdit.SetBitmaps(IDB_BITMAP4,RGB(255,255,255));
 	m_ButPrintEdit.DrawTransparent(true);
 	m_ButPrintEdit.SetFlat(0);
+	m_ButPrintEdit.SetTooltipText("打印编辑");
+	m_ButPrintEdit.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrintParam.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrintParam.SubclassDlgItem(IDC_BUT_PRINT_PARAM,this);
+	/*m_ButPrintParam.SetBitmaps(IDB_BITMAP3,RGB(255,255,255));
 	m_ButPrintParam.DrawTransparent(true);
 	m_ButPrintParam.SetFlat(0);
+	m_ButPrintParam.SetAlign(CButtonST::ST_ALIGN_VERT);
+	*/
+	m_ButPrintParam.SetTooltipText("打印参数");
+	m_ButPrintParam.SetBitmaps(IDB_BITMAP5,RGB(255,255,255));
+	m_ButPrintParam.DrawBorder(FALSE);
+	m_ButPrintParam.SetFlat(1);
+//	m_ButPrintParam.SetColor(CButtonST::BTNST_COLOR_BK_OUT, RGB(255,0,0));
+//	m_ButPrintParam.SetColor(CButtonST::BTNST_COLOR_BK_IN, RGB(0,208,0));
+//	m_ButPrintParam.SetColor(CButtonST::BTNST_COLOR_BK_FOCUS, RGB(0,0,208));
 
-	m_ButPrintClean.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+
+
+	m_ButPrintClean.SubclassDlgItem(IDC_BUT_CLEAN,this);
+	m_ButPrintClean.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrintClean.DrawTransparent(true);
 	m_ButPrintClean.SetFlat(0);
+	m_ButPrintClean.SetTooltipText("喷头清洗");
+	m_ButPrintClean.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrintControl.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrintControl.SubclassDlgItem(IDC_BUT_PRINT_CONTROL,this);
+	m_ButPrintControl.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrintControl.DrawTransparent(true);
 	m_ButPrintControl.SetFlat(0);
+	m_ButPrintControl.SetTooltipText("打印控制");
+	m_ButPrintControl.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrintCounter.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrintCounter.SubclassDlgItem(IDC_BUT_COUNTER,this);
+	m_ButPrintCounter.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrintCounter.DrawTransparent(true);
 	m_ButPrintCounter.SetFlat(0);
+	m_ButPrintCounter.SetTooltipText("打印计数");
+	m_ButPrintCounter.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrintIcon.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrintIcon.SubclassDlgItem(IDC_BUT_ICON,this);
+	m_ButPrintIcon.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrintIcon.DrawTransparent(true);
 	m_ButPrintIcon.SetFlat(0);
+	m_ButPrintIcon.SetTooltipText("打印图标");
+	m_ButPrintIcon.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrintClass.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrintClass.SubclassDlgItem(IDC_BUT_CLASS_SETTING,this);
+	m_ButPrintClass.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrintClass.DrawTransparent(true);
 	m_ButPrintClass.SetFlat(0);
+	m_ButPrintClass.SetTooltipText("班次设定");
+	m_ButPrintClass.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrintSystem.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrintSystem.SubclassDlgItem(IDC_BUT_SYSTEM,this);
+	m_ButPrintSystem.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrintSystem.DrawTransparent(true);
 	m_ButPrintSystem.SetFlat(0);
+	m_ButPrintSystem.SetTooltipText("系统设置");
+	m_ButPrintSystem.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	m_ButPrinterManage.SetBitmaps(IDB_BITMAP1,RGB(255,255,255),IDB_BITMAP1,RGB(255,255,255));
+	m_ButPrinterManage.SubclassDlgItem(IDC_BUT_PRINTER_MANAGE,this);
+	m_ButPrinterManage.SetBitmaps(IDB_BITMAP1,RGB(255,255,255));
 	m_ButPrinterManage.DrawTransparent(true);
 	m_ButPrinterManage.SetFlat(0);
+	m_ButPrinterManage.SetTooltipText("设备管理");
+	m_ButPrinterManage.SetAlign(CButtonST::ST_ALIGN_VERT);
 
-	//m_dlgPrinterManage->ShowWindow(SW_SHOW);
+	m_dlgPrinterManage->ShowWindow(SW_SHOW);
 
 }
 
@@ -279,17 +296,14 @@ void CBigCharPrinterView::ShowDialogByID(int ID)
 	{
 		GetDlgItem(IDC_STATIC_DIA_NAME)->SetWindowTextA("设备管理");
 		m_dlgPrinterManage->ShowWindow(SW_SHOW);
-	}
-	
+	}	
 }
-
 
 void CBigCharPrinterView::OnBnClickedButPrintEdit()
 {
 	// TODO: 在此添加控件通知处理程序代码
 
 	ShowDialogByID(IDD_DIA_PRINT_EDIT);
-
 }
 
 void CBigCharPrinterView::OnBnClickedButPrintParam()
@@ -298,13 +312,11 @@ void CBigCharPrinterView::OnBnClickedButPrintParam()
 	ShowDialogByID(IDD_DIA_PRINT_PARAM);
 }
 
-
 void CBigCharPrinterView::OnBnClickedButIcon()
 {
 	// TODO: 在此添加控件通知处理程序代码
 	ShowDialogByID(IDD_DIA_CUSTOM_ICON);
 }
-
 
 void CBigCharPrinterView::OnBnClickedButSystem()
 {
@@ -312,13 +324,11 @@ void CBigCharPrinterView::OnBnClickedButSystem()
 	ShowDialogByID(IDD_DIA_SYSTEM_SETTING);
 }
 
-
 void CBigCharPrinterView::OnBnClickedButClean()
 {
 	// TODO: 在此添加控件通知处理程序代码
 	ShowDialogByID(IDD_DIA_MOUTH_CLEAN);
 }
-
 
 void CBigCharPrinterView::OnBnClickedButPrintControl()
 {
@@ -326,27 +336,20 @@ void CBigCharPrinterView::OnBnClickedButPrintControl()
 	ShowDialogByID(IDD_DIA_PRINT_CONTROL);
 }
 
-
 void CBigCharPrinterView::OnBnClickedButCounter()
 {
 	// TODO: 在此添加控件通知处理程序代码
-
 	 ShowDialogByID(IDD_DIA_COUNTER);
-
 }
-
 
 void CBigCharPrinterView::OnBnClickedButClassSetting()
 {
 	// TODO: 在此添加控件通知处理程序代码
 	ShowDialogByID(IDD_DIA_CLASS_SETTING);
-
 }
-
 
 void CBigCharPrinterView::OnBnClickedButPrinterManage()
 {
 	// TODO: 在此添加控件通知处理程序代码
 	ShowDialogByID(IDD_DIA_PRINTER_MANAGE);
-
 }
--- BigCharPrinter/DiaPrintEdit.cpp
+++ BigCharPrinter/DiaPrintEdit.cpp
@@ -14,7 +14,8 @@ IMPLEMENT_DYNAMIC(CDiaPrintEdit, CDialogEx)
 CDiaPrintEdit::CDiaPrintEdit(CWnd* pParent /*=NULL*/)
 	: CDialogEx(CDiaPrintEdit::IDD, pParent)
 {
-
+	m_bDesign = true; 
+	m_nStepPixels = 5;
 }
 
 CDiaPrintEdit::~CDiaPrintEdit()
@@ -24,11 +25,179 @@ CDiaPrintEdit::~CDiaPrintEdit()
 void CDiaPrintEdit::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_COM_PARAM, m_ComParam);
+	DDX_Control(pDX, IDC_COM_DATA, m_ComData);
+	DDX_Control(pDX, IDC_STATIC_DESIGN_AREA, m_designArea);
 }
 
 
 BEGIN_MESSAGE_MAP(CDiaPrintEdit, CDialogEx)
+	ON_WM_PAINT()
+	ON_BN_CLICKED(IDC_BUT_DESIGN_VIEW, &CDiaPrintEdit::OnBnClickedButDesignView)
+	ON_BN_CLICKED(IDC_BUT_PRINT_VIEW, &CDiaPrintEdit::OnBnClickedButPrintView)
+	ON_BN_CLICKED(IDC_BUT_DESIGN_CLEAN, &CDiaPrintEdit::OnBnClickedButDesignClean)
+	ON_BN_CLICKED(IDC_BUT_DESIGN_SAVE, &CDiaPrintEdit::OnBnClickedButDesignSave)
+	ON_BN_CLICKED(IDC_BUT_OPEN_DESIGN, &CDiaPrintEdit::OnBnClickedButOpenDesign)
+	ON_BN_CLICKED(IDC_BUT_INSERT_PARAM, &CDiaPrintEdit::OnBnClickedButInsertParam)
+	ON_BN_CLICKED(IDC_BUT_INSERT_DADA, &CDiaPrintEdit::OnBnClickedButInsertDada)
 END_MESSAGE_MAP()
 
 
 // CDiaPrintEdit 消息处理程序
+
+
+BOOL CDiaPrintEdit::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  在此添加额外的初始化
+
+	m_ComParam.SendMessage(CB_SETITEMHEIGHT,-1,43); //改变组合框的高度
+	m_ComData.SendMessage(CB_SETITEMHEIGHT,-1,43);
+
+	m_ComParam.SendMessage(CB_SETITEMHEIGHT,0,20);
+	m_ComData.SendMessage(CB_SETITEMHEIGHT,0,20);
+
+	for (int i = 1; i <= 10; i++)
+	{
+		CString str;
+		str.Format("%d",i);
+		m_ComParam.InsertString(i-1,str);
+	}
+	m_ComParam.SetCurSel(0);
+
+	m_ComData.InsertString(0,"系统日期");
+	m_ComData.InsertString(1,"有效日期");
+	m_ComData.InsertString(2,"系统时间");
+	m_ComData.InsertString(3,"计数1");
+	m_ComData.InsertString(4,"计数2");
+	m_ComData.InsertString(5,"计数3");
+	m_ComData.InsertString(6,"字母计数");
+	m_ComData.InsertString(7,"班次码");
+
+	m_ComData.SetCurSel(0);	 
+	m_designArea.SetWindowPos(NULL,-1,-1,195*5,16*5, SWP_NOMOVE | SWP_NOACTIVATE | SWP_NOZORDER);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 异常: OCX 属性页应返回 FALSE
+}
+
+
+void CDiaPrintEdit::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+	// TODO: 在此处添加消息处理程序代码
+	// 不为绘图消息调用 CDialogEx::OnPaint()
+
+	CDC* pDC = m_designArea.GetDC();
+	if(m_bDesign)
+	{
+		//先清除打印预览内容
+		CBrush cbrush;
+		CBrush* pBrush; //旧笔刷
+		CRect rect;
+		m_designArea.GetClientRect(&rect);
+		cbrush.CreateSolidBrush(RGB(255,255,255)); 
+		pBrush=pDC->SelectObject(&cbrush); //载入笔刷
+		pDC->Rectangle(&rect); //绘制矩形		
+		pDC->SelectObject(pBrush); //恢复笔刷
+		cbrush.DeleteObject();
+		pBrush->DeleteObject();
+
+		//绘制设计结果
+
+	}
+	else
+	{
+		 
+		CPen cPen; 
+		cPen.CreatePen(PS_SOLID,1,RGB(252,157,154)); 
+		CPen* pOldPen; 
+		pOldPen = pDC->SelectObject(&cPen); //载入笔刷
+		
+		for(int i = 0; i < m_nRowSum; i++)
+		{
+			pDC->MoveTo(0,i*m_nStepPixels);
+			pDC->LineTo(m_nStepPixels*m_nColSum,i*m_nStepPixels);
+		}
+		for(int i = 0; i < m_nColSum; i++)
+		{
+			pDC->MoveTo(i*m_nStepPixels,0);
+			pDC->LineTo(i*m_nStepPixels,m_nStepPixels*m_nRowSum);
+		}
+		pDC->SelectObject(pOldPen);
+		cPen.DeleteObject();
+		pOldPen->DeleteObject();
+
+		CBrush cbrush;
+		CBrush* pBrush; //旧笔刷
+		CRect rect;
+		m_designArea.GetClientRect(&rect);
+		cbrush.CreateSolidBrush(RGB(0,0,0)); 
+		pBrush=pDC->SelectObject(&cbrush); //载入笔刷
+
+	   //pDC->Rectangle(&rect); //绘制矩形
+		for(int i = 0; i < m_nRowSum; i++)
+			for(int j = 0; j < m_nColSum; j++)
+				DrawOnePoint(pDC,i,j);
+
+		pDC->SelectObject(pBrush); //恢复笔刷
+		cbrush.DeleteObject();
+		pBrush->DeleteObject();
+	}
+
+	ReleaseDC(pDC); 
+}
+
+void CDiaPrintEdit::OnBnClickedButDesignView()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	m_bDesign = true; 
+	OnPaint();
+}
+
+void CDiaPrintEdit::OnBnClickedButPrintView()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	m_bDesign = false;
+ 	OnPaint();
+}
+
+void CDiaPrintEdit::DrawOnePoint(CDC* pDC ,int row,int col)
+{
+	// TODO: 在此添加控件通知处理程序代码
+
+	CRect rect(col*m_nStepPixels,row*m_nStepPixels,(col+1)*m_nStepPixels,(row+1)*m_nStepPixels);
+
+	pDC->Ellipse(&rect); 
+}
+
+
+void CDiaPrintEdit::OnBnClickedButDesignClean()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void CDiaPrintEdit::OnBnClickedButDesignSave()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void CDiaPrintEdit::OnBnClickedButOpenDesign()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void CDiaPrintEdit::OnBnClickedButInsertParam()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void CDiaPrintEdit::OnBnClickedButInsertDada()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
