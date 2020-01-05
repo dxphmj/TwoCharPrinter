cec1641a3c7fa9caafde405f78cdc9d7095c2092
dxphmj <36809189+dxphmj@users.noreply.github.com>
2020-01-05 17:22:28 +0800

    test2

---
+++ HighResolutionPrinter/GeneratedFiles/ui_filemanageform.h
@@ -0,0 +1,50 @@
+/********************************************************************************
+** Form generated from reading UI file 'filemanageform.ui'
+**
+** Created: Sun Jan 5 16:03:49 2020
+**      by: Qt User Interface Compiler version 4.8.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_FILEMANAGEFORM_H
+#define UI_FILEMANAGEFORM_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_FileManageForm
+{
+public:
+
+    void setupUi(QWidget *FileManageForm)
+    {
+        if (FileManageForm->objectName().isEmpty())
+            FileManageForm->setObjectName(QString::fromUtf8("FileManageForm"));
+        FileManageForm->resize(400, 300);
+
+        retranslateUi(FileManageForm);
+
+        QMetaObject::connectSlotsByName(FileManageForm);
+    } // setupUi
+
+    void retranslateUi(QWidget *FileManageForm)
+    {
+        FileManageForm->setWindowTitle(QApplication::translate("FileManageForm", "Form", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class FileManageForm: public Ui_FileManageForm {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_FILEMANAGEFORM_H
---
+++ HighResolutionPrinter/GeneratedFiles/ui_paramsettingform.h
@@ -0,0 +1,50 @@
+/********************************************************************************
+** Form generated from reading UI file 'paramsettingform.ui'
+**
+** Created: Sun Jan 5 16:03:49 2020
+**      by: Qt User Interface Compiler version 4.8.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_PARAMSETTINGFORM_H
+#define UI_PARAMSETTINGFORM_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ParamSettingForm
+{
+public:
+
+    void setupUi(QWidget *ParamSettingForm)
+    {
+        if (ParamSettingForm->objectName().isEmpty())
+            ParamSettingForm->setObjectName(QString::fromUtf8("ParamSettingForm"));
+        ParamSettingForm->resize(400, 300);
+
+        retranslateUi(ParamSettingForm);
+
+        QMetaObject::connectSlotsByName(ParamSettingForm);
+    } // setupUi
+
+    void retranslateUi(QWidget *ParamSettingForm)
+    {
+        ParamSettingForm->setWindowTitle(QApplication::translate("ParamSettingForm", "Form", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ParamSettingForm: public Ui_ParamSettingForm {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_PARAMSETTINGFORM_H
