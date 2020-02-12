1b3e3b9b345cce161b3369ff6be4d7310cafc985
chuweicheng <747392362@qq.com>
2020-02-12 17:32:40 +0800

    修改了序列号的一部分功能

--- HighResolutionPrinter/fileeditchild.h
+++ HighResolutionPrinter/fileeditchild.h
@@ -56,9 +56,8 @@ public:
 	QString SerialNumber_2;
 	int SerialNumber;
 	int SerialNumber_length;
-	int SerialNumber_r[10];//存放序列号数据
-	int SerialNumber_p;//指向序列号数组的指针
 	int drawback;//首次撤回的判定
+	int drawback_time;
 	int Barheight;
 	QPoint pointMousePressed;//记录鼠标点
 	bool boolMousePressed;//判断鼠标是否被按下
